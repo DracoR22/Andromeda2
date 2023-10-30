572106968076c6b8ddb850198fe060f59aec4a65
DracoR22 <rdraco039@gmail.com>
2023-10-29 21:35:08 -0500

    cors13

--- server/app.js
+++ server/app.js
@@ -19,28 +19,10 @@ const app = express()
 
 app.use(express.json({ limit: "50mb" }))
 app.use(cookieParser())
-
-// Set middleware of CORS 
-app.use((req, res, next) => {
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      "https://andromeda-pearl.vercel.app/"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Private-Network", true);
-    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-    res.setHeader("Access-Control-Max-Age", 7200);
-  
-    next();
-  });
+app.use(cors({
+    origin: ['https://andromeda-pearl.vercel.app'],
+    credentials: true
+}))
 
 // Test Api
 app.use("/", (req, res) => {
