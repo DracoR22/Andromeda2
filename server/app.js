ef99bac43b7916adaa138d5a8ee555a95d0fea58
DracoR22 <rdraco039@gmail.com>
2023-10-29 19:10:24 -0500

    corsfix

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ const app = express()
 app.use(express.json({ limit: "50mb" }))
 app.use(cookieParser())
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://andromeda-pearl.vercel.app'],
     credentials: true
 }))
 
