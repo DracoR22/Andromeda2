35a084a27db27f02d489ee00d1498e2d0cd5e890
DracoR22 <rdraco039@gmail.com>
2023-10-29 19:04:34 -0500

    nextauthfixdeploy

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth, { AuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || '',
