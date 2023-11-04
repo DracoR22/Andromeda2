0085060af298d713c24ed08701e48d81155a0eee
DracoR22 <rdraco039@gmail.com>
2023-11-04 16:56:12 -0500

    ui2

--- frontend/components/hero/Hero.tsx
+++ frontend/components/hero/Hero.tsx
@@ -1,13 +1,13 @@
-import hero1 from "@/public/hero/hero1.png"
-import hero2 from "@/public/hero/hero2.jpg"
-import hero3 from "@/public/hero/heroshop.jpg"
+import hero1 from "@/public/hero/heros1.webp"
+import hero2 from "@/public/hero/heros2.webp"
+import hero3 from "@/public/hero/heros3.webp"
 import hero4 from "@/public/hero/hero4.jpg"
 import ImageSlider from "./ImageSlider"
 
 const IMAGES = [
-    { url: hero2, alt: "Hero 1" },
-    { url: hero3, alt: "Hero 2" },
-    { url: hero1, alt: "Hero 3" },
+    { url: hero1, alt: "Hero 1" },
+    { url: hero2, alt: "Hero 2" },
+    { url: hero3, alt: "Hero 3" },
     { url: hero4, alt: "Hero 4" }
 ]
 
